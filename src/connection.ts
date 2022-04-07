0219b909f066113c7613895270cb64a73b25ad1b
xavigrausebastian <xavier.grau@kreatings.com>
2022-04-07 12:28:25 +0200

    Change mongoose import

--- src/connection.ts
+++ src/connection.ts
@@ -1,4 +1,4 @@
-import * as mongoose from "mongoose";
+import mongoose from "mongoose";
 
 // TODO: Try to find which type is app variable
 export function initDatabase(PORT: number, URI: string, app: any) {
