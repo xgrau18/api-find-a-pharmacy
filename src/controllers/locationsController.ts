7ba342b82308073de1994c7ebd3df427729fb1e2
Jofre_triquell <jtriquell21@jaumebalmes.net>
2022-04-19 12:45:45 +0200

    vb

--- src/controllers/locationsController.ts
+++ src/controllers/locationsController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
+import { type } from "os";
+import { send } from "process";
 
 
-const Order = require("../models/modelJson");
+
+const feature = require("../models/modelJson");
 
 
 
 export function mataro_ph(req: Request, res: Response) {
-         
+   
+   const aux=feature.find({},);
+   res.send("hola");
+
 }
